58d1f0cb64149089eaf2ef555f181063bc5f3b42
Maryna-Amrakhova <seagull2305@gmail.com>
2024-10-02 11:36:47 +0300

    added js and UX

--- src/main.js
+++ src/main.js
@@ -14,12 +14,12 @@ function generateBreakfast(event) {
   let apiKey = "btf3a0c41c82o04bde5657e178c809b4";
   let prompt = `User instructions: Generate a breakfast with ${userInstruction.value}`;
   let context =
-    "You are a great expert and love to cook useful and helthy food. Your mission is to generate a quick and nutritious breakfast in 4-5 rows and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the breakfast. Sign the breakfast with 'SheCodes AI' inside a <strong> element.";
+    "Ви великий майстер і любите готувати корисну та здорову їжу. Ваше завдання — створити швидкий і поживний сніданок на 4-5 рядків і розділити кожен рядок <br />. Обов’язково дотримуйтесь інструкцій користувача. Не додавайте назву до сніданку. Підпишіть сніданок за допомогою «Ваш SheCodes AI» всередині елемента <strong>.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemBreakfast = document.querySelector("#breakfast");
   poemBreakfast.classList.remove("hidden");
-  poemBreakfast.innerHTML = `<div class="generating"> Generating a breakfast with ${userInstruction.value}</div>`;
+  poemBreakfast.innerHTML = `<div class="generating"> Зачекайте, я генерую Вам сніданок з ${userInstruction.value}</div>`;
 
   axios.get(apiUrl).then(displayBreakfast);
 }
